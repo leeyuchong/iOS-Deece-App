819298b63d9cf522d97f192d774d63659871ff4a
leeyuchong <leeyuchong@outlook.com>
2021-03-22 20:08:14 -0400

    Initial Commit

---
+++ iOS-Deece/iOS_DeeceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_DeeceApp.swift
+//  iOS-Deece
+//
+//  Created by Lee Yuchong on 22/3/21.
+//
+
+import SwiftUI
+
+@main
+struct iOS_DeeceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
